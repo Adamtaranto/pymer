352bf35770869bca26c61f5a7edbb826088355d3
Kevin Murray <kdmfoss@gmail.com>
2017-03-02 22:19:53 +1100

    Declare source UTF8 coding to un-break py2

--- pymer/tests.py
+++ pymer/tests.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 # Copyright 2016 Kevin Murray <kdmfoss@gmail.com>
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 import numpy as np
 from tempdir import run_in_tempdir
