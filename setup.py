20c09352d44b0591b4eefbc6dc4696439149f8f5
Adam Taranto <adam.p.taranto@gmail.com>
2025-01-24 22:31:27 +1100

    Use pyproject.toml and hatch for build

--- setup.py
+++ setup.py
@@ -1,108 +1,27 @@
-#!/usr/bin/env python3
-#
-# Copyright 2016 Kevin Murray <spam@kdmurray.id.au>
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+"""
+Package setup for Cython extensions only.
 
-from setuptools import setup
-import versioneer
-from setuptools.extension import Extension
-from setuptools.command.test import test as TestCommand
-from setuptools.command.build_ext import build_ext as OrigBuildExt
+This script sets up the Cython extensions for the pymer package.
 
-EXT = 'c'
-try:
-    from Cython.Build import cythonize
-    EXT = 'pyx'
-except ImportError:
-    def cythonize(x): return x
+"""
 
-description = "pymer: Pythonic fast k-mer counting routines"
+from Cython.Build import build_ext
+from setuptools import setup, Extension
+import numpy as np
 
+ext_modules = [
+    Extension(
+        "pymer._hash",
+        ["src/pymer/_hash.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-O3", "-march=native"],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    ),
+]
 
-class NoseCommand(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # Run nose ensuring that argv simulates running nosetests directly
-        import nose
-        nose.run_exit(argv=['nosetests'])
-
-
-class BuildExt(OrigBuildExt):
-
-    def finalize_options(self):
-        OrigBuildExt.finalize_options(self)
-
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-
-
-cmdclasses = versioneer.get_cmdclass()
-cmdclasses['test'] = NoseCommand
-cmdclasses['build_ext'] = BuildExt
+cmdclass = {"build_ext": build_ext}
 
 setup(
-    name='pymer',
-    packages=['pymer', ],
-    version=versioneer.get_version(),
-    cmdclass=cmdclasses,
-    install_requires=[
-        'h5py',
-        'scipy',
-        'screed>=1.0',
-    ],
-    setup_requires=[
-        'numpy>=1.10',
-        'nose',
-    ],
-    tests_require=[
-        'nose-cov',
-        'blessings',
-        'docopt',
-        'tempdir',
-    ],
-    ext_modules=cythonize([
-        Extension(
-            'pymer._hash', [
-                'pymer/_hash.{}'.format(EXT),
-            ],
-        ),
-    ]),
-    description=description,
-    author="Kevin Murray",
-    author_email="kdmfoss@gmail.com",
-    url="https://github.com/kdmurray91/pymer",
-    keywords=["kmer"],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-    ],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
 )
